3ecc33d9c446a7a8c32c150d382836fc25c4a98c
arsh <114301032+Lana-Asadi@users.noreply.github.com>
2026-02-12 23:19:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-function buyNow() {
-    alert("Checkout coming soon!");
+let cartCount = 0;
+
+function changeImage(src) {
+  document.getElementById("main-image").src = src;
+}
+
+document.getElementById("main-image").addEventListener("click", function() {
+  document.getElementById("lightbox").style.display = "flex";
+  document.getElementById("lightbox-img").src = this.src;
+});
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
+
+function addToCart() {
+  cartCount++;
+  document.getElementById("cart-count").innerText = cartCount;
+  alert("Added to cart!");
+}
+
+function checkout() {
+  window.location.href = "https://buy.stripe.com/test_123456789";
 }
